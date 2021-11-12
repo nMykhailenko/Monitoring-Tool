39851330c82d7cbb6da07cac3e77039cf80321b2
Nikita Mykhailenko <nikita.mykhailenko@soulve.nu>
2021-11-12 16:08:33 +0200

    refactoring + unit test

--- tests/MonitoringTool.Infrastructure.UnitTests/Services/ConnectedClientServiceTests.cs
+++ tests/MonitoringTool.Infrastructure.UnitTests/Services/ConnectedClientServiceTests.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
@@ -14,6 +15,7 @@ using MonitoringTool.Domain.Entities;
 using MonitoringTool.Infrastructure.Services;
 using Moq;
 using Xunit;
+using Xunit.Abstractions;
 
 namespace MonitoringTool.Infrastructure.UnitTests.Services
 {
@@ -30,6 +32,39 @@ namespace MonitoringTool.Infrastructure.UnitTests.Services
             _mapper = mapperConfiguration.CreateMapper();
 
             _connectedClientRepositoryMock = new Mock<IConnectedClientRepository>();
+        }
+        
+        [Fact]
+        public async Task GetActiveAsync_ShouldReturn_Only_ConnectedClients_With_Active_Status()
+        {
+            // arrange
+            const int connectedServicesPerClientCount = 3;
+            var expectedConnectedClient = new Faker<ConnectedClient>()
+                .RuleFor(cc => cc.Name, _ => _.Name.FullName())
+                .RuleFor(cc => cc.IsActive, _ => true)
+                .RuleFor(cc
+                    => cc.ConnectedServices, _ => new Faker<ConnectedService>()
+                    .RuleFor(cs => cs.BaseUrl, _ => _.Internet.UrlRootedPath())
+                    .RuleFor(cs => cs.Name, _ => _.Name.FullName())
+                    .Generate(connectedServicesPerClientCount))
+                .Generate(connectedServicesPerClientCount);
+            
+            _connectedClientRepositoryMock
+                .Setup(x
+                    => x.GetActiveAsync(It.IsAny<CancellationToken>()))
+                .ReturnsAsync(expectedConnectedClient);
+
+            _sut = new ConnectedClientService(_mapper, _connectedClientRepositoryMock.Object);
+            
+            // act
+            var actual = await _sut.GetActiveAsync(CancellationToken.None);
+            
+            // assert
+            actual.Count().Should().Be(expectedConnectedClient.Count);
+            actual
+                .Select(x => x.IsActive)
+                .Should()
+                .Equal(expectedConnectedClient.Select(c => c.IsActive));
         }
 
         [Fact]
@@ -105,7 +140,7 @@ namespace MonitoringTool.Infrastructure.UnitTests.Services
         }
 
         [Fact]
-        public async Task AddConnectedClient_ShouldReturn_AddedConnectedClient()
+        public async Task AddConnectedClient_ShouldReturn_AddedConnectedClient_If_ConnectedClient_With_Defined_Name_Not_Exists()
         {
             // arrange
             const int connectedServicesPerClientCount = 3;
@@ -168,7 +203,7 @@ namespace MonitoringTool.Infrastructure.UnitTests.Services
         }
 
         [Fact]
-        public async Task AddConnectedClient_ShouldReturn_EntityAlreadyExistResponse()
+        public async Task AddConnectedClient_ShouldReturn_EntityAlreadyExistResponse_If_ConnectedClient_With_Defined_Name_Already_Exists()
         {
             // arrange
             const string expectedCode = "EntityIsAlreadyExists";
@@ -205,6 +240,119 @@ namespace MonitoringTool.Infrastructure.UnitTests.Services
             var actualModel = (EntityIsAlreadyExists)actual.Value;
             actualModel.Message.Should().BeEquivalentTo(expectedErrorMessage);
             actualModel.Code.Should().BeEquivalentTo(expectedCode);
+        }
+
+        [Fact]
+        public async Task
+            AddConnectedServicesToClientAsync_ShouldReturn_EntityNotFoundResponse_If_ConnectedClient_With_Defined_Name_Not_Exists()
+        {
+            // arrange 
+            const string expectedCode = "EntityNotFound";
+            const string connectedClientName = "test name";
+            var expectedErrorMessage = $"Connected client with name: {connectedClientName} not found.";
+
+            var createConnectedServiceRequests = new List<CreateConnectedServiceRequest>();
+            ConnectedClient? currentConnectedClient = null;
+            _connectedClientRepositoryMock
+                .Setup(x
+                    => x.GetByNameAsync(connectedClientName, It.IsAny<CancellationToken>()))
+                .ReturnsAsync(currentConnectedClient);
+            
+            _sut = new ConnectedClientService(_mapper, _connectedClientRepositoryMock.Object);
+
+            // act 
+            var actual = await _sut.AddConnectedServicesToClientAsync(
+                connectedClientName, 
+                createConnectedServiceRequests,
+                CancellationToken.None);
+            
+            // assert
+            actual.Value.Should().BeOfType(typeof(EntityNotFoundResponse));
+            
+            var actualModel = (EntityNotFoundResponse)actual.Value;
+            actualModel.Message.Should().BeEquivalentTo(expectedErrorMessage);
+            actualModel.Code.Should().BeEquivalentTo(expectedCode);
+        }
+
+        [Fact]
+        public async Task
+            AddConnectedServicesToClientAsync_ShouldReturn_ConnectedServiceResponses_If_ConnectedClient_With_Defined_Name_Exists()
+        {
+            // arrange 
+            const int connectedServicesPerClientCount = 5;
+            const int connectedServiceId = 5;
+            const int connectedClientId = 2;
+            const string connectedClientName = "test name";
+            
+            var expectedConnectedClient = new Faker<ConnectedClient>()
+                .RuleFor(cc => cc.Id, _ => connectedClientId)
+                .RuleFor(cc => cc.Name, _ => _.Name.FullName())
+                .RuleFor(cc
+                    => cc.ConnectedServices, _ => new Faker<ConnectedService>()
+                    .RuleFor(cs => cs.ConnectedClientId, _ => connectedClientId)
+                    .RuleFor(cs => cs.BaseUrl, _ => _.Internet.UrlRootedPath())
+                    .RuleFor(cs => cs.Name, _ => _.Name.FullName())
+                    .RuleFor(cs => cs.IsActive, _ => true)
+                    .Generate(connectedServicesPerClientCount))
+                .Generate();
+            var notActiveConnectedService = new Faker<ConnectedService>()
+                .RuleFor(cs => cs.Id, _ => connectedServiceId)
+                .RuleFor(cs => cs.ConnectedClientId, _ => connectedClientId)
+                .RuleFor(cs => cs.BaseUrl, _ => _.Internet.UrlRootedPath())
+                .RuleFor(cs => cs.Name, _ => _.Name.FullName())
+                .RuleFor(cs => cs.IsActive, _ => false)
+                .Generate();
+            expectedConnectedClient.ConnectedServices.Add(notActiveConnectedService);
+            
+            var createConnectedServiceRequests = new List<CreateConnectedServiceRequest>();
+            var createConnectedServiceRequestIsNotActive =
+                expectedConnectedClient.ConnectedServices
+                    .Where(x => !x.IsActive)
+                    .Select(x => new CreateConnectedServiceRequest
+                    {
+                        Name = x.Name,
+                        BaseUrl = x.BaseUrl
+                    }).First();
+            var createConnectedServiceRequestIsActive =
+                expectedConnectedClient.ConnectedServices
+                    .Where(x => x.IsActive)
+                    .Select(x => new CreateConnectedServiceRequest
+                    {
+                        Name = x.Name,
+                        BaseUrl = x.BaseUrl
+                    }).First();
+            createConnectedServiceRequests.Add(createConnectedServiceRequestIsActive);
+            createConnectedServiceRequests.Add(createConnectedServiceRequestIsNotActive);
+
+            _connectedClientRepositoryMock
+                .Setup(x
+                    => x.GetByNameAsync(connectedClientName, It.IsAny<CancellationToken>()))
+                .ReturnsAsync(expectedConnectedClient);
+
+            _connectedClientRepositoryMock
+                .Setup(x
+                    => x.AddConnectedServiceAsync(It.IsAny<ConnectedService>(), CancellationToken.None))
+                .ReturnsAsync(notActiveConnectedService);
+            
+            _sut = new ConnectedClientService(_mapper, _connectedClientRepositoryMock.Object);
+
+            
+            // act 
+            var actualResponse = await _sut.AddConnectedServicesToClientAsync(
+                connectedClientName,
+                createConnectedServiceRequests,
+                CancellationToken.None);
+
+            actualResponse.Value.Should().BeOfType(typeof(List<ConnectedServiceResponse>));
+            
+            var actualModel = (List<ConnectedServiceResponse>)actualResponse.Value;
+            actualModel.Should().HaveCount(1);
+
+            var actualConnectedClientResponse = actualModel.First();
+            actualConnectedClientResponse.Id.Should().Be(connectedServiceId);
+            actualConnectedClientResponse.IsActive.Should().Be(true);
+            actualConnectedClientResponse.Name.Should().BeEquivalentTo(createConnectedServiceRequestIsNotActive.Name);
+            actualConnectedClientResponse.BaseUrl.Should().BeEquivalentTo(createConnectedServiceRequestIsNotActive.BaseUrl);
         }
     }
 }
